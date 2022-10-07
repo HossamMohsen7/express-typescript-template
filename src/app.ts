9cb90a89029e0add3e810444f1f820338b98850a
Hossam Mohsen <6911267+HossamMohsen7@users.noreply.github.com>
2022-10-07 23:09:55 +0200

    Init

---
+++ src/app.ts
@@ -0,0 +1,56 @@
+import "reflect-metadata";
+import "dotenv/config";
+import express from "express";
+import { createServer } from "http";
+import helmet from "helmet";
+import hpp from "hpp";
+import cors from "cors";
+import morgan from "morgan";
+
+import { helloRouter } from "./routes/helloRoute.js";
+
+const app = express();
+const http = createServer(app);
+
+const corsOptions = {
+  origin: (origin: any, callback: (err: any, origin?: any) => void) => {
+    callback(null, origin);
+  },
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+const PORT = process.env.PORT || 3000;
+
+app.set("trust proxy", "loopback");
+app.use(
+  helmet({
+    crossOriginEmbedderPolicy: false,
+    crossOriginOpenerPolicy: false,
+    crossOriginResourcePolicy: false,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(hpp());
+app.use(cors(corsOptions));
+app.use(morgan("combined"));
+
+app.use(helloRouter);
+
+app.get("/", (req, res) => {
+  return res.status(200).send("Everything is working great!");
+});
+
+const startServer = () => {
+  console.log("Starting server..");
+  http.listen(PORT, () => {
+    console.log("listening on *:" + PORT);
+  });
+};
+
+const main = async () => {
+  startServer();
+};
+
+main();
